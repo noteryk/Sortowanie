bc0dc64839a6b745e895d1a654ba383fc7a38aff
noteryk <74415945+noteryk@users.noreply.github.com>
2024-03-12 22:46:43 +0100

    Add project files.

---
+++ Sortowanie/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 8, 5, 7, 3, 1 };
+
+        Console.WriteLine("Przed sortowaniem: ");
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Sortowanie tablicy za pomocą InsertionSort
+        InsertionSort(numbers);
+
+        Console.WriteLine("Po sortowaniu: ");
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void InsertionSort(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
